70fe803a8e677d22f8178e95f4056bcde469c05f
Khairul Haaziq <rhycoz@yahoo.com>
2023-03-26 12:29:58 +0800

    add

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,9 @@
-<template>
 
+import { Script } from 'vm';
+
+<template>
+        <Head>
+                <Script async defer data-website-id="4194166d-1053-455c-af17-19f8a103e4d4" src="https://umami-production-134d.up.railway.app/umami.js"></Script>
+        </Head>
         <div class="text-neutral-800"><slot/></div>
 </template>
